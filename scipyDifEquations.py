c930708bc21612e03d7db752b8af87a39e7a41d5
India Daniel <72756858+igdaniel-1@users.noreply.github.com>
2025-11-25 15:32:26 -0500

    modelling another diff equation

--- scipyDifEquations.py
+++ scipyDifEquations.py
@@ -5,22 +5,40 @@ import numpy as np
 from scipy.integrate import odeint 
 import matplotlib.pyplot as plt 
 
-def differential_equation(y,t): 
+def differential_equation1(y,t): 
     dydt = -y * t + 5
     return dydt
+def differential_equation2(y,t): 
+    dydt = y**2 * t + 8 * t
+    return dydt
 
-# initial condition 
-y0 = 1
-  
-# values of time 
-t = np.linspace(0,5) 
-  
-# Solving ODE 
-y = odeint(differential_equation, y0, t) 
+def testDiffEquation():
+    # initial condition 
+    y0 = 1
+    
+    # values of time 
+    t = np.linspace(0,5) 
+    
+    # Solving ODE 
+    y = odeint(differential_equation, y0, t) 
 
-# plot DE
-plt.plot(t,y) 
-plt.xlabel("Time") 
-plt.ylabel("Y") 
-plt.title("Visualizing dy/dt ODE")
-plt.show()
+    # plot DE
+    plt.plot(t,y) 
+    plt.xlabel("Time") 
+    plt.ylabel("Y") 
+    plt.title("Visualizing dy/dt ODE")
+    plt.show()
+
+def testAnotherDiffEquation():
+    y0 = 1
+    t = np.linspace(1,2)
+    # dydt = differential_equation2(y0, t)
+    y = odeint(differential_equation2, y0, t) 
+    
+    plt.plot(t,y)
+    plt.xlabel("Time")
+    plt.ylabel("Y")
+    plt.title("Visualizing dy/dt ODE")
+    plt.show()
+    
+testAnotherDiffEquation()
