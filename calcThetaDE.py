9d8a9669f12b5ffe29d29fd4fb70ea84a8b82092
India Daniel <72756858+igdaniel-1@users.noreply.github.com>
2025-12-03 15:46:04 -0500

    diffy eq work

--- calcThetaDE.py
+++ calcThetaDE.py
@@ -1,5 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+import sys
+
+# BUG LOG: 
+# 
+# There's an issue trying to call the solve_ivp function of the theta values
+### The error portion of the code I believe is:
+# theta_0 = np.pi / 3
+# theta_values = solve_ivp(thetaDoubleDot, [0,t], theta_0, t_eval=time_scale)
+# solver = method(fun, t0, y0, tf, vectorized=vectorized, **options)
+# 
+### The error is as follows:
+# raise ValueError("`y0` must be 1-dimensional.")
+# 
+# I don't know why it's reading theta_0 as a multi-dimensional object
+# The type is float, the length is 1
+
 
 # physical constants
 g = 9.8
@@ -9,6 +26,7 @@ mu = 0.1
 # initial constraints
 theta_0 = np.pi / 3  # 60 degrees
 theta_dot0 = 0      # no initial angular velocity
+delta_t = 0.01
 
 # defining the DE
 def thetaDoubleDot(theta, theta_dot):
@@ -18,7 +36,7 @@ def thetaDoubleDot(theta, theta_dot):
 def theta(t):
     theta = theta_0
     theta_dot  = theta_dot0
-    delta_t = 0.01
+    
 
     # how theta changes with respect to time
     for time in np.arange(0,t,delta_t):
@@ -33,25 +51,31 @@ def theta(t):
 
     return theta
 
-def main():
-    t = 1
+def valueAtTime(t):
     print("Theta at time", t, "seconds:",theta(t))
 
-# main()
 
 def plotter(t):
     time_scale = np.linspace(0,t,1)
+    # time_scale = np.linspace(0,t,delta_t)
+    print('theta_0:',theta_0)
+
+    # value for theta over interval [0,t)
+    theta_values = solve_ivp(thetaDoubleDot, [0,t], 3, t_eval=time_scale)
+
     plt.figure(figsize=(t,3))
     # theta definition
 
-    for second in range(t):
+    for second in range(len(theta_values.t)):
         print("second:", second, "theta:", theta(second))
-        plt.plot(second, theta(second), label='Theta')
+        plt.plot(time_scale, theta_values.y[:,second], label='Theta')
     plt.xlabel('Time')
     plt.ylabel('Theta')
     plt.title('Theta changes over time')
     plt.grid(True)
     plt.show()
 
+
+# testing and execution
 # number passed into plotter is experiment duration in seconds 
-plotter(10)
+# plotter(10)
