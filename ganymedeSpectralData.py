65648ffea20ea06952f4836dde468b51a1453838
India Daniel <72756858+igdaniel-1@users.noreply.github.com>
2025-12-11 13:02:31 -0500

    checking fits data types

--- ganymedeSpectralData.py
+++ ganymedeSpectralData.py
@@ -18,4 +18,19 @@ import numpy as np
 
 file = fits.open('./ganymede.fits')
 file.info()
-print('\nheader:',file[0].header[0:5])
+header = file[0].header
+print('\nheader:',header[0:5])
+
+print("\nnaxis:",header["NAXIS"])  # this is the data size
+print("\nnaxis:",header["NAXIS1"])  # this is the data size of axis 1
+print("\nCRVAL1:",header["CRVAL1"])
+print("\nDATe:",header["DATE"])
+print("\nBUNIT:",header["BUNIT"])  # what the hell is relative flux
+
+# CRVAL1
+
+
+# flux = np.random.randn(200) * u.Jy
+# wavelength = np.arange(5100, 5300) * u.AA
+# spectra = Spectrum1D(spectral_axis=wavelength, flux=flux)
+# spectra = Spectrum1D.read(file)
