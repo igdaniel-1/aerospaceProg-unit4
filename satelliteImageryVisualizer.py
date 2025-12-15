6e71ee4c137abac895a1189b134cccab1419de19
India Daniel <72756858+igdaniel-1@users.noreply.github.com>
2025-12-15 17:57:38 -0500

    display band TIFF data

--- satelliteImageryVisualizer.py
+++ satelliteImageryVisualizer.py
@@ -7,3 +7,25 @@ import rasterio as rio
 import earthpy as et
 import earthpy.spatial as es
 import earthpy.plot as ep
+import matplotlib as plt
+
+# Importing Band TIFF Data
+# only one file is in the folder ATM
+# need to download and add the other TIFF files
+np.seterr(divide='ignore', invalid='ignore')
+bands = glob("./satellite-bands/*B?*.tiff")
+bands.sort()
+
+data = []
+for band in bands:
+    with rio.open(band, 'r') as file:
+        data.append(file.read(1))
+stack_data = np.stack(data)
+ep.plot_bands(stack_data, cmap = 'gist_earth', figsize = (20, 12), cols = 6, cbar = False)
+# plt.show()
+
+# stack the rgb data
+rgb = ep.plot_rgb(stack_data, 
+                  rgb=(3,2,1), 
+                  figsize=(10, 16))
+plt.show()
